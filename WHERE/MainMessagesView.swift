c9900b96dfbd7f78f7a52be8136c964feb900a52
teenteennnii <teennopwijit@gmail.com>
2024-04-15 13:41:06 +0700

    refactor

--- WHERE/MainMessagesView.swift
+++ WHERE/MainMessagesView.swift
@@ -55,54 +55,61 @@ struct MainMessagesView: View {
             VStack {
                 
                 customNavBar
-                
-                ScrollView {
-                    ForEach(0..<10, id: \.self) {num in
-                        VStack {
-                            HStack(spacing: 16){
-                                Image(systemName: "person.fill")
-                                    .font(.system(size: 32))
-                                    .padding(8)
-                                    .overlay(RoundedRectangle(cornerRadius: 44).stroke(Color(.label), lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/))
-                                
-                                VStack(alignment: .leading) {
-                                    Text("Username")
-                                        .font(.system(size: 14, weight: .bold))
-                                    Text("Message sent to user")
-                                        .font(.system(size: 14))
-                                        .foregroundColor(Color(.lightGray))
-                                }
-                                Spacer()
-                                
-                                Text("22d")
-                                    .font(.system(size: 14, weight: .semibold))
-                            }
-                            Divider()
-                                .padding(.vertical, 8)
-                        }
-                        .padding(.horizontal)
-                    }
-                    .padding(.bottom, 50)
-                }
+                messagesView
                 
             }
-            .overlay(Button {
-                
-            } label: {
-                HStack {
-                    Spacer()
-                    Text("+ New Message")
-                        .font(.system(size: 16, weight: .bold))
-                    Spacer()
-                }
-                .foregroundColor(.white)
-                .padding(.vertical)
-                    .background(Color.blue)
-                    .cornerRadius(32)
-                    .padding(.horizontal)
-                    .shadow(radius: 15)
-            }, alignment: .bottom)
+            .overlay(newMessageButton, alignment: .bottom)
             .navigationBarHidden(true)
+        }
+    }
+    
+    private var messagesView: some View {
+        ScrollView {
+            ForEach(0..<10, id: \.self) {num in
+                VStack {
+                    HStack(spacing: 16){
+                        Image(systemName: "person.fill")
+                            .font(.system(size: 32))
+                            .padding(8)
+                            .overlay(RoundedRectangle(cornerRadius: 44).stroke(Color(.label), lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/))
+                        
+                        VStack(alignment: .leading) {
+                            Text("Username")
+                                .font(.system(size: 14, weight: .bold))
+                            Text("Message sent to user")
+                                .font(.system(size: 14))
+                                .foregroundColor(Color(.lightGray))
+                        }
+                        Spacer()
+                        
+                        Text("22d")
+                            .font(.system(size: 14, weight: .semibold))
+                    }
+                    Divider()
+                        .padding(.vertical, 8)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom, 50)
+        }
+    }
+    
+    private var newMessageButton: some View {
+        Button {
+            
+        } label: {
+            HStack {
+                Spacer()
+                Text("+ New Message")
+                    .font(.system(size: 16, weight: .bold))
+                Spacer()
+            }
+            .foregroundColor(.white)
+            .padding(.vertical)
+                .background(Color.blue)
+                .cornerRadius(32)
+                .padding(.horizontal)
+                .shadow(radius: 15)
         }
     }
 }
